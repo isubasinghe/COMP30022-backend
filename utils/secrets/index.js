1aed0975e25f1528eb2f5a4be9d7b622db64b5a9
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-08-19 10:35:55 +1000

    Added initial config helpers

---
+++ utils/secrets/index.js
@@ -0,0 +1,15 @@
+let cache = [];
+
+
+const getValue = (key) => {
+    let value = cache[key];
+    if (value === undefined) {
+        if(process.env[key]) {
+            return process.env[key];
+        }else {
+            throw new Error("Environment variable does not exist");
+        }
+    }
+    return value;
+}   
+module.exports = getValue;
