24f782066073206958bc3afbda3de47719cc1fc3
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-08-20 11:09:37 +1000

    Removed sequelize

--- models/db/index.js
+++ models/db/index.js
@@ -1,16 +1,1 @@
 "use strict";
-const Sequelize = require("sequelize");
-let sequalize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB.USER,
-  process.env.DB_PASSS,
-  {
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 2,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
