8008b856b9052c1bf21625b655bd6a6176b1bee7
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-08-23 23:19:34 +1000

    Added source code for database connectivty establishment

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 "use strict";
+const db = require('./models/db');
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const app = express();
+app.locals.db = db; 
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
--- models/db/index.js
+++ models/db/index.js
@@ -1,1 +1,31 @@
 "use strict";
+
+const secrets = require('../../utils/secrets');
+
+const PG_URL=secrets('DB_URL');
+const PG_PORT=secrets('DB_PORT');
+const PG_DATABASE=secrets('DB_DATABASE');
+const PG_USER=secrets('DB_USER');
+const PG_PWD=secrets('DB_PWD');
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: PG_URL,
+        port: PG_PORT,
+        database: PG_DATABASE,
+        user: PG_USER,
+        password: PG_PWD,
+    },
+    pool: {
+        min: 2,
+        max: 3
+    }
+});
+
+knex.on('disconnect', err => {
+    console.log(err);
+    throw err;
+})
+
+module.exports = {knex: knex};
