6967e555243ece0202973965cac07a9665b5bcb1
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-08-23 23:34:52 +1000

    Added logDNA

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 "use strict";
+const logger = require('./utils/logger');
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const httpLogger = require("morgan");
 
 const app = express();
-
-app.use(logger("dev"));
+app.locals.logger = logger;
+app.use(httpLogger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
