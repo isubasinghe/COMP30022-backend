bcbd17140c12801dea893f47324ce0f0436dec3d
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-09-02 22:02:02 +1000

    Added user read commands

--- models/db/user/index.js
+++ models/db/user/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const db = require('../../db');
+
+const read = async (email) => {
+  return db.knex('userdata')
+    .select()
+    .where({ email });
+}
+
+const update = async (email, name) => {
+  return db.knex('userdata')
+    .where({ email })
+    .update({ name });
+}
+
+
+module.exports = { read, update };
