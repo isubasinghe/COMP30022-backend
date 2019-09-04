68f15fb1be24c97ba1cf2f5f884f5c04a6ee4d2a
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-09-04 11:04:05 +1000

    Added Admin/Member requirements for read/update/del/etc.

--- models/db/artifact/index.js
+++ models/db/artifact/index.js
@@ -2,41 +2,57 @@
 
 const db = require('../../db');
 
-const create = (register_id, name, family_members, description, date, lat, lon) => {
-  return db.knex('artifact')
-    .insert({ register_id, name, family_members, description, date, lat, lon });
+const create = (email, register_id, name, family_members, description, date, lat, lon) => {
+  return db.knex('membership')
+    .where({ 'email': email, register_id, 'is_admin': true })
+    .then((data) => {
+      if (data && data.length !== 0) {
+        return db.knex('artifact')
+          .insert({ register_id, name, family_members, description, date, lat, lon });
+      }
+    });
 }
 
-const read = (artifact_id) => {
-  return readArtifactsWithPhotos()
+const read = (email, register_id, artifact_id) => {
+  return readAll(email, register_id)
     .where('artifact.artifact_id', artifact_id);
 }
 
-const readRegisterArtifacts = (register_id) => {
-  return readArtifactsWithPhotos() 
-    .where({ register_id });
-}
-
-const update = (artifact_id, name, family_members, description, date, lat, lon) => { 
-  return db.knex('artifact') 
-    .where({ artifact_id })
-    .update({ name, family_members, description, date, lat, lon });
-}
-
-const del = (artifact_id) => {
-  return db.knex('artifact')
-    .where({ artifact_id })
-    .del();
-}
-
-const readArtifactsWithPhotos = () => {
+const readAll = (email, register_id) => {
   return db.knex('artifact')
   .leftOuterJoin('photo', 'artifact.artifact_id', 'photo.artifact_id')
   .select(
     'artifact.*',
     db.knex.raw('json_agg(photo.*) as photos') 
   )    
-  .groupBy('artifact.artifact_id');
+  .groupBy('artifact.artifact_id')
+  .whereIn('register_id', function() {
+    this.select('register_id')
+      .from('membership')
+      .where({ email, register_id })
+  });
 }
 
-module.exports = { create, read, readRegisterArtifacts, update, del };
+const update = (email, register_id, artifact_id, name, family_members, description, date, lat, lon) => {   
+  return db.knex('artifact') 
+    .update({ name, family_members, description, date, lat, lon })
+    .where({ artifact_id })
+    .whereExists(function() {
+      this.select()
+        .from('membership')
+        .where({ email, register_id, 'is_admin': true })
+    });
+}
+
+const del = (email, register_id, artifact_id) => {
+  return db.knex('artifact')
+    .where({ artifact_id })
+    .whereExists(function() {
+      this.select()
+        .from('membership')
+        .where({ email, register_id, 'is_admin': true })
+    })
+    .del();
+}
+
+module.exports = { create, read, readAll, update, del };
