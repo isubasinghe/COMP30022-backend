db43c929ab02700e3d9b9674290476206fb96929
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-10 12:53:59 +1000

    Added cors

--- app.js
+++ app.js
@@ -6,16 +6,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const httpLogger = require("morgan");
 const app = express();
+const cors = require("cors");
 app.locals.logger = logger;
 app.locals.db = db;
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.use(httpLogger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
