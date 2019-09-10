199341d30b34cda13a7b59b463f9b856a595c510
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-10 16:22:27 +1000

    Added error handler

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ const cookieParser = require("cookie-parser");
 const httpLogger = require("morgan");
 const app = express();
 const cors = require("cors");
+const Sentry = require("@sentry/node");
+
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+
 app.locals.logger = logger;
 app.locals.db = db;
+app.use(Sentry.Handlers.requestHandler());
 app.use(cors());
 app.use(httpLogger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/", routes);
-
+app.use(Sentry.Handlers.errorHandler());
 module.exports = app;
