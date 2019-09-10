b20f0ab24d809e6857dda9f0c25cc76fd850c166
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-10 11:07:32 +1000

    Added auth in routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 const v1 = require("./v1");
 const authMiddleware = require("../../middleware/auth");
 
-//router.use(authMiddleware);
+router.use(authMiddleware);
 router.use("/v1", v1);
 
 module.exports = router;
