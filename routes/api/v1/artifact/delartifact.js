2ecaa02d0274f9649e00230fbea0e21a4fb1ae61
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-09-11 12:26:40 +1000

    status, not send

--- routes/api/v1/artifact/delartifact.js
+++ routes/api/v1/artifact/delartifact.js
@@ -18,7 +18,7 @@ const delArtifact = (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      res.send(400).json({ error: err.message });
+      res.status(400).json({ error: err.message });
     });
   
 };
