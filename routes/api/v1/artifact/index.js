00fddfbb56710770348780d7e665f80076a4cdd5
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-09-11 12:30:29 +1000

    Fixed route

--- routes/api/v1/artifact/index.js
+++ routes/api/v1/artifact/index.js
@@ -2,9 +2,10 @@
 const router = require("express").Router();
 const addArtifact = require("./addartifact");
 const delArtifact = require("./delartifact");
+const updateArtifact = require("./updateartifact");
 
 router.post("/add", addArtifact);
 router.post("/del", delArtifact);
-router.post("/update", delArtifact);
+router.post("/update", updateArtifact);
 
 module.exports = router;
