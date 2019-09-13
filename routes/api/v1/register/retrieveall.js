cb4f13ab1eacf070f08217c2b198c7c14925816f
Isitha Subasinghe <isubasinghe@pop-os.localdomain>
2019-09-13 11:43:23 +1000

    Removed registerId from return json

--- routes/api/v1/register/retrieveall.js
+++ routes/api/v1/register/retrieveall.js
@@ -12,7 +12,7 @@ const retrieveArtifacts = (req, res) => {
   req.app.locals.db.artifact
     .readAll(res.locals.authenticatedEmail, registerId)
     .then(data => {
-      res.send({ ...data, register_id: registerId });
+      res.send({ ...data });
     })
     .catch(err => {
       console.log(err);
