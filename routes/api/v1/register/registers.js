4420e373b94f2bde2ddaaf9e6d7f960e53c19cb6
James Edward Barnes <barnesj2@student.unimelb.edu.au>
2019-09-20 19:37:07 +1000

    removed unnecessary validation

--- routes/api/v1/register/registers.js
+++ routes/api/v1/register/registers.js
@@ -1,16 +1,6 @@
 "use strict";
 
 const registers = (req, res) => {
-  const { email, register_id, is_admin } = req.body;
-
-  [email, register_id, is_admin]
-  .forEach(element => {
-    if (element === undefined) {
-      status(400).json({ error: "incorrect schema" });      
-      return;
-    }
-  });
-
   req.app.locals.db.register
     .readUserRegisters(res.locals.authenticatedEmail)
     .then(() => {
