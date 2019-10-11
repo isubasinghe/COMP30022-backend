90b6c67792449f36ed5b700e744574ac52bc785f
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-11 17:46:34 +1100

    integrated log analysis and api analytics

--- utils/logger/index.js
+++ utils/logger/index.js
@@ -6,7 +6,6 @@ require("winston-papertrail").Papertrail;
 const logger = new winston.transports.Papertrail({
   host: secrets("PAPER_TRAIL_HOST"),
   port: parseInt(secrets("PAPER_TRAIL_PORT")),
-  colorize: true,
   handleExceptions: true
 });
 module.exports = logger;
