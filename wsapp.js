454af858e82f9a99fb01ffe3ae5347a6119dc86d
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-10-24 22:51:49 +1100

    Added authentication for websocket endpoint

--- wsapp.js
+++ wsapp.js
@@ -1,6 +1,7 @@
 const ws = require("ws");
 const url = require("url");
-
+const db = require("./models/db");
+const getUser = require("./utils/auth/cognito");
 const wss = new ws.Server({
   noServer: true
 });
@@ -10,6 +11,22 @@ wss.on("connection", (conn, req) => {
     ? req.headers["x-forwarded-for"].split(/\s*,\s*/)[0]
     : req.connection.remoteAddress;
   const token = url.parse(req.url, true).query.token || "no_token";
+  getUser(token)
+    .then(user => {
+      if (!(user instanceof Error)) {
+        console.log(user);
+        return db.register.readUserRegisters(user);
+      } else {
+        throw new Error(user.message);
+      }
+    })
+    .then(registers => {
+      if (registers.length > 0) {
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = wss;
